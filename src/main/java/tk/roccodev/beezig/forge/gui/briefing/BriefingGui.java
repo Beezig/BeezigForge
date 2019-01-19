8d14f403135faecdadf11e8500bb16c4eabc04f9
RoccoDev <roccodev.business@gmail.com>
2019-01-19 12:58:19 +0100

    Laby tabs support

--- src/main/java/tk/roccodev/beezig/forge/gui/briefing/BriefingGui.java
+++ src/main/java/tk/roccodev/beezig/forge/gui/briefing/BriefingGui.java
@@ -1,6 +1,7 @@
 package tk.roccodev.beezig.forge.gui.briefing;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -8,10 +9,12 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import tk.roccodev.beezig.forge.gui.briefing.tabs.TabRenderer;
 
 import java.io.IOException;
+import java.util.List;
 
 public class BriefingGui extends GuiScreen {
 
     private TabRenderer render;
+    private Class labyTabs;
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
@@ -22,8 +25,15 @@ public class BriefingGui extends GuiScreen {
 
     @Override
     public void initGui() {
+        try {
+            labyTabs = Class.forName("net.labymod.gui.elements.Tabs");
+            labyTabs.getMethod("initGuiScreen", List.class, GuiScreen.class)
+                    .invoke(null, buttonList, this);
+        }
+        catch(Exception ignored) {}
         this.render = new TabRenderer(width, height);
         super.initGui();
+
     }
 
     public void show() {
@@ -46,6 +56,17 @@ public class BriefingGui extends GuiScreen {
     public void handleMouseInput() throws IOException {
         super.handleMouseInput();
         render.onMouseInput();
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) throws IOException {
+        super.actionPerformed(button);
+        try {
+            if(labyTabs != null)
+                labyTabs.getMethod("actionPerformedButton", GuiButton.class)
+                        .invoke(null, button);
+        }
+        catch (Exception ignored) {}
     }
 
     @SubscribeEvent
