e258509cdaa460971d2e4f38ab9b82555f508f02
RoccoDev <roccodev.business@gmail.com>
2019-10-12 19:02:52 +0200

    Version bump

--- src/main/java/eu/beezig/forge/BeezigForgeMod.java
+++ src/main/java/eu/beezig/forge/BeezigForgeMod.java
@@ -66,7 +66,7 @@ public class BeezigForgeMod {
 
     public static final String MODID = "BeezigForge";
     public static final String NAME = "Beezig Forge Expansion";
-    public static final String VERSION = "6.1.0";
+    public static final String VERSION = "6.2.0";
 
 
     private boolean handlerLoaded;
