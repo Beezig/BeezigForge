b1677b53a409fd70e8ed1f876169cc3e495945ed
Archer <archer@beezig.eu>
2020-10-31 21:57:14 +0100

    rc: Bump version to 7.0.0-rc1
    Signed-off-by: Archer <archer@beezig.eu>

--- src/main/java/eu/beezig/forge/BeezigForgeMod.java
+++ src/main/java/eu/beezig/forge/BeezigForgeMod.java
@@ -60,7 +60,7 @@ public class BeezigForgeMod {
 
     public static final String MODID = "BeezigForge";
     public static final String NAME = "Beezig Forge Expansion";
-    public static final String VERSION = "7.0.0";
+    public static final String VERSION = "7.0.0-rc1";
 
 
     private boolean handlerLoaded;
